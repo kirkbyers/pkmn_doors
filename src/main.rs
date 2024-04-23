82aead84526f648851d48a66fa35715e45c3dd34
kirkbyers <kirk@cratejoy.com>
2024-04-22 23:03:34 -0500

    Doors close on cmd+w.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
-use rdev::{listen, Event, EventType};
+use rdev::{listen, Event, EventType, Key, Keyboard, KeyboardState};
 use rodio::Sink;
 use rodio::{Decoder, OutputStream};
 use std::io::Cursor;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Err(error) = listen(cb) {
+    if let Err(error) = listen(cb()) {
         println!("Error {:?}", error);
     }
 
     Ok(())
 }
 
-fn cb(e: Event) {
-    if e.event_type == EventType::ButtonPress(rdev::Button::Left) {
-        tokio::spawn(async {
-            play_doors();
-        });
+fn cb() -> impl FnMut(Event) {
+    let mut cmd_pressed = false;
+    move |e: Event| {
+        // println!("{:?}", e);
+        if e.event_type == EventType::KeyPress(Key::MetaLeft)
+            || e.event_type == EventType::KeyPress(Key::MetaRight)
+        {
+            cmd_pressed = true;
+        }
+        if e.event_type == EventType::KeyRelease(Key::MetaLeft)
+            || e.event_type == EventType::KeyRelease(Key::MetaRight)
+        {
+            cmd_pressed = false;
+        }
+        if e.event_type == EventType::KeyPress(Key::KeyW) && cmd_pressed {
+            tokio::spawn(async {
+                play_doors();
+            });
+        }
     }
 }
 
