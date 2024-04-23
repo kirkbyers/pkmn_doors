599e508a9a4df61a79f21c92d90ab8660dd468fa
kirkbyers <kirk@cratejoy.com>
2024-04-22 21:32:45 -0500

    Play multiple sounds at the same time.

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,40 @@ use rodio::Sink;
 use rodio::{Decoder, OutputStream};
 use std::io::Cursor;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Err(error) = listen(cb) {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Err(error) = listen(cb_2) {
         println!("Error {:?}", error);
     }
 
     Ok(())
 }
 
+fn cb_2(e: Event) {
+    if e.event_type == EventType::ButtonPress(rdev::Button::Left) {
+        tokio::spawn(async {
+            play_doors();
+        });
+    }
+}
+
+fn play_doors() {
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let sink = Sink::try_new(&stream_handle).unwrap();
+    // Load a sound from a file, using a path relative to Cargo.toml
+    let slice = Cursor::new(
+        include_bytes!("./Pokémon Red_Blue_Yellow - Door Enter - Sound Effect-00rlTif_Kfg.flac")
+            .as_ref(),
+    );
+    // Decode that sound file into a source
+    let source = Decoder::new(slice).unwrap();
+
+    sink.append(source);
+    sink.sleep_until_end();
+}
+
 fn cb(e: Event) {
     if e.event_type == EventType::ButtonPress(rdev::Button::Left) {
-        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-        let sink = Sink::try_new(&stream_handle).unwrap();
-        // Load a sound from a file, using a path relative to Cargo.toml
-        let slice = Cursor::new(
-            include_bytes!(
-                "./Pokémon Red_Blue_Yellow - Door Enter - Sound Effect-00rlTif_Kfg.flac"
-            )
-            .as_ref(),
-        );
-        // Decode that sound file into a source
-        let source = Decoder::new(slice).unwrap();
-
-        sink.append(source);
-        sink.sleep_until_end();
+        play_doors();
     }
 }
