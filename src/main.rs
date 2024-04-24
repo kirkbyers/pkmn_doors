0c4e3d47f7028caea5d1f4386f5cf46252495174
kirkbyers <kirk@cratejoy.com>
2024-04-23 22:51:05 -0500

    Add pkmn center sound on cmd+s.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rdev::{listen, Event, EventType, Key, Keyboard, KeyboardState};
+use rdev::{listen, Event, EventType, Key};
 use rodio::Sink;
 use rodio::{Decoder, OutputStream};
 use std::io::Cursor;
@@ -33,21 +33,37 @@ fn cb() -> impl FnMut(Event) {
                 play_doors();
             });
         }
+        if e.event_type == EventType::KeyPress(Key::KeyS) && cmd_pressed {
+            tokio::spawn(async {
+                play_pkmn_center();
+            });
+        }
     }
 }
 
-fn play_doors() {
+fn play_bytes(bytes: &Vec<u8>) {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
     sink.set_volume(VOL);
     // Load a sound from a file, using a path relative to Cargo.toml
-    let slice = Cursor::new(
-        include_bytes!("./Pokémon Red_Blue_Yellow - Door Enter - Sound Effect-00rlTif_Kfg.flac")
-            .as_ref(),
-    );
+    let slice = Cursor::new(bytes.to_vec());
     // Decode that sound file into a source
     let source = Decoder::new(slice).unwrap();
 
     sink.append(source);
     sink.sleep_until_end();
+}
+
+fn play_doors() {
+    play_bytes(
+        &include_bytes!("./Pokémon Red_Blue_Yellow - Door Enter - Sound Effect-00rlTif_Kfg.flac")
+            .to_vec(),
+    )
+}
+
+fn play_pkmn_center() {
+    play_bytes(
+        &include_bytes!("./Pokémon Center Heal - Pokémon Red_Blue_Yellow Version-3IQSjLXfiPI.flac")
+            .to_vec(),
+    )
 }
